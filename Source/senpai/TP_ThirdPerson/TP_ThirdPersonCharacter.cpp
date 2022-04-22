87a201f1da0420987638e5c069a39ae3edcd74c1
Florentin Rieger <florentin.rieger@gmail.com>
2022-04-22 03:30:57 +0200

    feat(player character): started to work on hit animation
    - added mixamo animation
    - added right hand collision sphere
    - play animation on hit button click

--- Source/senpai/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
+++ Source/senpai/TP_ThirdPerson/TP_ThirdPersonCharacter.cpp
@@ -49,6 +49,8 @@ ATP_ThirdPersonCharacter::ATP_ThirdPersonCharacter()
 
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named ThirdPersonCharacter (to avoid direct content references in C++)
+
+	UE_LOG(LogTemp, Warning, TEXT("ThirdPersonCharacter Constructor"));
 }
 
 //////////////////////////////////////////////////////////////////////////
