da5b47d4bafdf070ddb0e1872752aa4f9fbac1f1
PSchlagnitweit <74833687+PSchlagnitweit@users.noreply.github.com>
2022-09-29 11:31:37 +0200

    feat(AImodel): adapt reinforcement model to new enemy tags

--- Source/senpai/AI_model/ReinforceModel.cpp
+++ Source/senpai/AI_model/ReinforceModel.cpp
@@ -13,9 +13,9 @@ UReinforceModel::UReinforceModel()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-    add2Layer(0, { 0,1,7 });
-    add2Layer(1, { 2,3 });
-    add2Layer(2, { 4,5,6,7 });
+    add2Layer(0, { 0,1,2 });
+    add2Layer(1, { 3,4 });
+    add2Layer(2, { 5,6,7 });
     initQtable();
 }
 
