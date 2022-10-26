d5837bd4936b57d09c4ba72afe849120fee3fd3e
chrisclemenz <chris.clemenz@gmail.com>
2022-10-26 19:58:02 +0200

    feat(Stagger): implemented base stagger functions in cpp

--- Source/senpai/Components/BaseStaggerComponent.cpp
+++ Source/senpai/Components/BaseStaggerComponent.cpp
@@ -1,18 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "Components/BaseStaggerComponent.h"
+#include "BaseStaggerComponent.h"
 
 // Sets default values for this component's properties
 UBaseStaggerComponent::UBaseStaggerComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
 
 	// ...
 }
-
 
 // Called when the game starts
 void UBaseStaggerComponent::BeginPlay()
@@ -20,15 +18,25 @@ void UBaseStaggerComponent::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	CurrentStaggerValue = 0;
+
+	GetOwner()->OnTakeAnyDamage.AddDynamic(this, &UBaseStaggerComponent::StaggerActor);
 }
 
-
-// Called every frame
-void UBaseStaggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UBaseStaggerComponent::StaggerActor(AActor *DamagedActor, float Damage, const class UDamageType *DamageType, class AController *InstigatedBy, AActor *DamageCauser)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-
-	// ...
+	// Check Damage Type here for stagger multiplier
+	IncreaseStaggerValue(Damage);
 }
 
+void UBaseStaggerComponent::IncreaseStaggerValue(float Amount)
+{
+	CurrentStaggerValue -= Amount;
+	CurrentStaggerValue = FMath::Clamp(CurrentStaggerValue, 0.f, MaxStaggerValue);
+
+	if (CurrentStaggerValue > MaxStaggerValue)
+	{
+		OnFullStagger.Broadcast(GetOwner());
+		CurrentStaggerValue = 0;
+	}
+}
--- Source/senpai/Components/BaseStaggerComponent.h
+++ Source/senpai/Components/BaseStaggerComponent.h
@@ -3,29 +3,39 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "ActorComponent.h"
+#include "Components/ActorComponent.h"
 #include "BaseStaggerComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStaggerSignature, AActor *, StaggeredActor);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FStaggerChangedSignature, AActor *, StaggeredActor);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class SENPAI_API UBaseStaggerComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UBaseStaggerComponent();
+
+	UPROPERTY(BlueprintAssignable)
+	FStaggerSignature OnFullStagger;
+	UPROPERTY(BlueprintAssignable)
+	FStaggerChangedSignature OnStaggerValueChanged;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-	UPROPERTY(VisibleAnywhere, GetBlueprintReplicationCondition)
-	float maxStagger;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float MaxStaggerValue = 100;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	float CurrentStaggerValue = 0;
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	float DeteriorationRate = 1;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+	UFUNCTION()
+	void StaggerActor(AActor *DamagedActor, float Damage, const class UDamageType *DamageType, class AController *InstigatedBy, AActor *DamageCauser);
 
-		
+	void IncreaseStaggerValue(float Amount);
 };
